ed06280d982ca870b9a76d80d02f86fad0523a84
Popa-Robert <86589891+Popa-Robert@users.noreply.github.com>
2021-06-28 11:26:45 +0300

    Add files via upload

---
+++ internship-shop-vsc/src/models/product.js
@@ -0,0 +1,13 @@
+class Product{
+    constructor(productId,name,description,price,basePrice,image,categoryId){
+this.productId=productId;
+this.name=name
+this.description=description
+this.price=price
+this.basePrice=basePrice;
+this.categoryId=categoryId;
+this.image=image
+    }
+}
+
+export default Product;
